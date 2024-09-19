7902ad2a6e6081478d741d3280d9010289e89d2c
petrovviacheslav <lery66lery@yandex.ru>
2024-09-19 14:36:25 +0300

    first add

---
+++ src/main/java/org/example/Checker.java
@@ -0,0 +1,51 @@
+package org.example;
+
+import java.util.HashMap;
+
+public class Checker {
+    private HashMap<String, String> data;
+    private int x;
+    private float y;
+    private int r;
+
+    public Checker(HashMap<String, String> data) {
+        this.data = data;
+    }
+
+    public boolean validate() {
+        try {
+            x = Integer.parseInt(data.get("x"));
+            y = Float.parseFloat(data.get("y"));
+            r = Integer.parseInt(data.get("r"));
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public boolean isHit() {
+        return quarter1() || quarter3() || quarter4();
+    }
+
+    private boolean quarter1() {
+        if (x >= 0 && y >= 0) {
+            return Math.sqrt(this.x * this.x + this.y * this.y) <= (double) this.r / 2;
+        }
+        return false;
+    }
+
+
+    private boolean quarter3() {
+        if (x < 0 && y < 0) {
+            return (this.x >= -this.r) && (this.y >= -this.r);
+        }
+        return false;
+    }
+
+    private boolean quarter4() {
+        if (x < 0 && y >= 0) {
+            return (this.y >= (double) this.r / 2 + 0.5 * this.x);
+        }
+        return false;
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,76 @@
+package org.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.HashMap;
+
+import com.fastcgi.*;
+
+public class Main {
+
+    private static final String SUCCESS_RESPONSE = """
+            HTTP/1.1 200 OK
+            """;
+    private static final String FAIL_RESPONSE = """
+            HTTP/1.1 400 Bad Request
+            """;
+
+    private static final String BASE_RESPONSE = """
+            Access-Control-Allow-Origin: *
+            Connection: keep-alive
+            Content-Type: application/json
+            Content-Length: %d
+                        
+            %s
+            """;
+
+    public static void main(String[] args) {
+        FCGIInterface fcgi = new FCGIInterface();
+
+        while (fcgi.FCGIaccept() >= 0) {
+            Date nowDate = new Date();
+            HashMap<String, String> params = parse(FCGIInterface.request.params.getProperty("QUERY_STRING"));
+
+            Checker check = new Checker(params);
+
+            String response;
+
+            if (check.validate()) {
+                long time = new Date().getTime() - nowDate.getTime();
+                response = createJson(BASE_RESPONSE, String.format("{\"result\": %b, \"time\": %d}", check.isHit(), time));
+//                System.out.println((new Date()).getTime() - nowDate.getTime());
+            } else {
+                response = createJson(BASE_RESPONSE, "{\"error\": \" incorrect data\"}");
+//                System.out.println((new Date()).getTime() - nowDate.getTime());
+            }
+
+            System.out.println(response);
+        }
+    }
+
+    private static String createJson(String type, String answer) {
+        return String.format(type, answer.getBytes(StandardCharsets.UTF_8).length, answer);
+    }
+
+    private static HashMap<String, String> parse(String jsonStr) {
+        HashMap<String, String> params = new HashMap<String, String>();
+        String[] pairs = jsonStr.split("&");
+        for (String pair : pairs) {
+
+            String[] keyValue = pair.split("=");
+
+            if (keyValue.length > 1) {
+                params.put(keyValue[0], keyValue[1]);
+            } else {
+                params.put(keyValue[0], "");
+            }
+        }
+
+        return params;
+    }
+
+}
